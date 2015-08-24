e8a6db775c2524025437ff379ed0c627180ee532
Yohann PARISIEN <yohann.parisien@aufeminin.com>
2015-08-24 10:51:58 +0200

    Rajout de variables dans la configuration

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -8,7 +8,12 @@ return array(
 		//Set to fasle when robot is connected
 		'simulated' => true,
 	),
-	'virtualkeyboard' => array (
+	'virtualkeyboard' => array(
 		'enable'	=> false,	
+	),
+	'import_export' => array(
+		'upload_path'			=> __DIR__ . '/../../public/tmp/',
+		'import_archive_path'	=> __DIR__ . '/../../data/import_archives/',
+		'export_archive_path'	=> __DIR__ . '/../../data/export_archives/',
 	),
 );
