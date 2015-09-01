66a35e71b0713a0e6a602d2cf6dcb1d51839c49e
Yohann PARISIEN <yohann.parisien@aufeminin.com>
2015-09-01 14:41:34 +0200

    Suppression du module Tools (non utilisé)

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,7 @@
 return array(
 	// This should be an array of module namespaces used in the application.
 	'modules' => array(
-		'Start', 'Auth','Operator','Manager','Tools','Bufferspace','Logger',
+		'Start', 'Auth','Operator','Manager','Bufferspace','Logger',
 	),
 
 	// These are various options for the listeners attached to the ModuleManager
