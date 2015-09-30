9972505cf2f59ce3e96ff1719560c7d49f35e059
Yohann PARISIEN <yohann.parisien@aufeminin.com>
2015-10-01 00:27:22 +0200

    Modif du fichier de conf

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -11,16 +11,18 @@ return array(
 	 * Configuration du robot
 	 */
 	'robot' => array(
-		'simulated'		=> true, //Active (true) / Desactive (false) le mode de simulation
+		'simulated'		=> true,				//Active (true) / Desactive (false) le mode de simulation
 		'simulation'	=> array(
-			'init' => array( //Paramètres de la séquence de démarrage
-				'trytogood'			=> 1, //Nb d'essai avant OK
-				'hasmed' 			=> false, //Un médicament est chargé (true) ou non (false) dans le robot
-				'loadedmed'			=> 'GLU', //Nom DCI du médicament chargé dans le robot
-				'loadedsourcekit'	=> false, //Kit source chargé (true) ou non (false)
-				'sourcekitscanned'	=> false, //Kit source scanné (true) ou non (false)
+			'init' => array( 					//Paramètres de la séquence de démarrage
+				'trytogood'			=> 1,		//Nb d'essai avant OK
+				'robotinerror'		=> 0,		//Déamarrage du robot en erreur 1=Erreur 0=OK
+				'roboterrorcode'	=> 2,		//Code de démarrage renvoyé par le robot en cas  d'erreur au démarrage
+				'hasmed' 			=> false,	//Un médicament est chargé (true) ou non (false) dans le robot
+				'loadedmed'			=> 'GLU',	//Nom DCI du médicament chargé dans le robot
+				'loadedsourcekit'	=> false,	//Kit source chargé (true) ou non (false)
+				'sourcekitscanned'	=> false,	//Kit source scanné (true) ou non (false)
 				'sourcekitserial'	=> '90830284902384238904823098402398402', //Numéro de série du kit source chargé et scanné
-				'unit'				=> 'mbq', //Unit de mesure du robot mbq ou mci
+				'unit'				=> 'mbq',	//Unit de mesure du robot mbq ou mci
 			),
 		),
 	),
@@ -28,14 +30,14 @@ return array(
 	 * Configuration du clavier virtuel
 	 */
 	'virtualkeyboard' => array(
-		'enable'	=> false, //Clavier virtuel activé (true) ou non (false)
-		'size'		=> 1.55, //Taille du clavier
+		'enable'	=> false,	//Clavier virtuel activé (true) ou non (false)
+		'size'		=> 1.55,	//Taille du clavier
 	),
 	/**
 	 * Configuration des imports/exports de fichiers
 	 */
 	'import_export' => array(
-		'upload_path'			=> __DIR__ . '/../../public/tmp', //Dossier de réception des fichiers entrants
+		'upload_path'			=> __DIR__ . '/../../public/tmp',			//Dossier de réception des fichiers entrants
 		'import_archive_path'	=> __DIR__ . '/../../data/import_archives', //Dossier d'archive des fichiers entrants
 		'export_archive_path'	=> __DIR__ . '/../../data/export_archives', //Dossier d'archive des fichiers sortants
 	),
@@ -45,6 +47,6 @@ return array(
 	'timeout' => array(
 			'operator'		=> 60 * 60, //Temps d'inactivité avec lock des écrans opérateurs
 			'supervisor'	=> 60 * 60, //Temps d'inactivité avec lock des écrans superviseurs
-			'robot'			=> 1, //Temps d'attente entre les essais de démarrage
+			'robot'			=> 1, 		//Temps d'attente entre les essais de démarrage
 	),
 );
