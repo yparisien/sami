49579bbd86476e6b007eeff9af92f9e5f6eb7b25
Yohann Parisien <yohann.parisien@aufeminin.com>
2016-11-01 13:48:35 +0100

    Vue Drug ajout diluable

--- module/Manager/src/Manager/View/VDrug.php
+++ module/Manager/src/Manager/View/VDrug.php
@@ -6,6 +6,7 @@ class VDrug
 	public	$id;
 	public	$drug_name;
 	public	$dci;
+	public  $dilutable;
 	public	$radionuclide_name;
 	public  $nbExams;
 
@@ -14,6 +15,7 @@ class VDrug
 		$this->id = (!empty($data['id'])) ? $data['id'] : null;
 		$this->radionuclide_name = (!empty($data['radionuclide_name'])) ? $data['radionuclide_name'] : null;
 		$this->drug_name = (!empty($data['drug_name'])) ? $data['drug_name'] : null;
+		$this->dilutable = (!empty($data['dilutable']) && $data['dilutable'] == 1) ? true : false;
 		$this->dci = (!empty($data['dci'])) ? $data['dci'] : null;
 		$this->nbExams = (!empty($data['nbExams'])) ? $data['nbExams'] : 0;
 	}
